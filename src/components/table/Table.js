a8c68d2b85243497c6958009e3c9119f3c43b791
surikov01 <surikov01@icloud.com>
2020-10-06 15:55:54 -0400

    Table edit

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,9 +4,32 @@ import {createTable} from '@/components/table/table.template'
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
+    constructor($root) {
+      super ($root, {
+        listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+      }) 
+      
+    }
+
     toHTML() {
         // А тут будем вызывать метод createTable() Т.е. функцию из файла table.template.js
         // В скобки createTable() можно передавать количество строк и получать таблицу гораздо большего размера
         return createTable(20) // Тут будем оперировать обычными строчками которые будем генерировать     
+    }
+
+    onClick() {
+      console.log('click')
+    }
+
+    onMouseup() {
+      console.log('Up')
+    }
+
+    onMousedown(event) {
+      console.log('mousedown', event.target)
+    }
+
+    onMousemove () {
+      console.log('move')
     }
 }
