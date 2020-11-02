34ffe7cdb9faeec6369840b399870142f5ff7fa2
Ivan <surikoff.ivan@yandex.ru>
2020-11-02 22:24:25 +0500

    Add Remote Workstation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {Excel} from '@/components/excel/Excel'
-import {Header} from '@/components/header/Header'
-import {Toolbar} from '@/components/toolbar/Toolbar'
-import {Formula} from '@/components/formula/Formula'
-import {Table} from '@/components/table/Table'
-import './scss/index.scss'
-const excel = new Excel('#app', { 
-    components: [Header, Toolbar, Formula, Table] 
-}) 
-excel.render()
+import { Excel } from '@/components/excel/Excel';
+import { Header } from '@/components/header/Header';
+import { Toolbar } from '@/components/toolbar/Toolbar';
+import { Formula } from '@/components/formula/Formula';
+import { Table } from '@/components/table/Table';
+import './scss/index.scss';
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+excel.render(); // Coment 1
